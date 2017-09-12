d2295da1d4d16e99a62ba03206211b662f4416e9
Zhehai <zhehaizhu@gmail.com>
2017-09-11 21:32:06 -0700

    problem with marker xml

--- app/src/main/java/com/example/zhuzhehai/mymap/MainActivity.java
+++ app/src/main/java/com/example/zhuzhehai/mymap/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.zhuzhehai.mymap;
 
 import android.Manifest;
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
@@ -12,6 +13,7 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -19,7 +21,9 @@ import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.AutocompleteFilter;
+import com.google.android.gms.location.places.GeoDataApi;
 import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBuffer;
 import com.google.android.gms.location.places.PlaceDetectionApi;
 import com.google.android.gms.location.places.PlaceLikelihood;
 import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
@@ -28,8 +32,20 @@ import com.google.android.gms.location.places.ui.PlaceAutocomplete;
 import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.google.android.gms.location.places.ui.PlacePicker;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends FragmentActivity implements GoogleApiClient.OnConnectionFailedListener {
+import static android.R.attr.data;
+import static android.R.attr.mipMap;
+
+public class MainActivity extends AppCompatActivity
+        implements OnMapReadyCallback {
     private GoogleApiClient mGoogleApiClient;
 
 
@@ -40,41 +56,154 @@ public class MainActivity extends FragmentActivity implements GoogleApiClient.On
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // Retrieve the content view that renders the map.
+//        mGoogleApiClient = new GoogleApiClient
+//                .Builder(this)
+//                .addApi(Places.GEO_DATA_API)
+//                .addApi(Places.PLACE_DETECTION_API)
+//                .enableAutoManage(this, (GoogleApiClient.OnConnectionFailedListener) this)
+//                .build();
 
-        mGoogleApiClient = new GoogleApiClient
-                .Builder(this)
-                .addApi(Places.GEO_DATA_API)
-                .addApi(Places.PLACE_DETECTION_API)
-                .enableAutoManage(this, this)
-                .build();
+        if (googleServcesAvaliable()) {
+            Toast.makeText(this, "connect!!!!!!!", Toast.LENGTH_LONG).show();
+            setContentView(R.layout.activity_maps);
 
-
-        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
-        try {
-            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST);
-        } catch (GooglePlayServicesRepairableException e) {
-            e.printStackTrace();
-        } catch (GooglePlayServicesNotAvailableException e) {
-            e.printStackTrace();
+            //marker!!!!
+//            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+//                    .findFragmentById(R.id.map);
+//            mapFragment.getMapAsync(this);
+        } else {
+            Toast.makeText(this, "not connect", Toast.LENGTH_LONG).show();
         }
+        // Get the SupportMapFragment and request notification
+        // when the map is ready to be used.
+
+
+
+
+
+
+
+
+
+
+//        PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+//                try {
+//                    startActivityForResult(builder.build(MainActivity.this), PLACE_PICKER_REQUEST);
+//                } catch (GooglePlayServicesRepairableException e) {
+//                    e.printStackTrace();
+//                } catch (GooglePlayServicesNotAvailableException e) {
+//                    e.printStackTrace();
+//                }
+
+
+
+//          marker
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+//                .findFragmentById(R.id.map);
+//        mapFragment.getMapAsync(this);
+
+
+
+
+
+//        doAutoFragment(); // onPlaceSelected;
+//        AutoCompletedActvity(); // search autoCompelte;
+
 
     }
 
+    private void doAutoFragment() {
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                Toast.makeText(MainActivity.this, "Show selected place : " + place.getName(), Toast.LENGTH_LONG).show();
+                //todo marker
+
+
+//                Marker marker = GoogleMap.addMarker(new MarkerOptions().position(place.getLatLng()
+//                                                        ).title(place.getName()+""));
+//                marker.showInfoWindow();
+//                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+//                try {
+//                    startActivityForResult(builder.build(MainActivity.this), PLACE_PICKER_REQUEST);
+//                } catch (GooglePlayServicesRepairableException e) {
+//                    e.printStackTrace();
+//                } catch (GooglePlayServicesNotAvailableException e) {
+//                    e.printStackTrace();
+//                }
+
+//                Toast.makeText(MainActivity.this, "lalla", Toast.LENGTH_LONG).show();
+                Log.i(TAG, "onPlaceSelected Place: " + place.getName());
+            }
+
+
+            @Override
+            public void onError(Status status) {
+                // TODO:Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+        // filter!!!!
+
+        AutocompleteFilter typeFilter = new AutocompleteFilter.Builder()
+                .setTypeFilter(AutocompleteFilter.TYPE_FILTER_ADDRESS)
+                .build();
+
+        autocompleteFragment.setFilter(typeFilter);
+
+        autocompleteFragment.setBoundsBias(new LatLngBounds(
+                new LatLng(-33.880490, 151.184363),
+                new LatLng(-33.858754, 151.229596)));
+    }
+
+    public boolean googleServcesAvaliable(){
+        GoogleApiAvailability api = GoogleApiAvailability.getInstance();
+        int isAvaliable = api.isGooglePlayServicesAvailable(this);
+        if (isAvaliable == ConnectionResult.SUCCESS) {
+            return true;
+        } else if (api.isUserResolvableError(isAvaliable)) {
+            Dialog dialog = api.getErrorDialog(this, isAvaliable,0);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "not connect", Toast.LENGTH_LONG).show();
+        }
+        return false;
+    }
+
+    private void AutoCompletedActvity() {
+        try {
+            Intent intent =
+                    new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
+                            .build(this);
+            startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+        } catch (GooglePlayServicesRepairableException e) {
+            // TODO:Handle the error.
+        } catch (GooglePlayServicesNotAvailableException e) {
+            // TODO:Handle the error.
+        }
+    }
 
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == PLACE_PICKER_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                Place place = PlacePicker.getPlace(data, this);
-                String toastMsg = String.format("Place: %s", place.getName());
-                Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
-            }
-        }
+//        if (requestCode == PLACE_PICKER_REQUEST) {
+//            // select
+//            if (resultCode == RESULT_OK) {
+//                Toast.makeText(MainActivity.this, "l33333a", Toast.LENGTH_LONG).show();
+//                Place place = PlacePicker.getPlace(data, this);
+//                String toastMsg = String.format("Place: %s", place.getName());
+//                Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show();
+//            }
+//        }
         if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE) {
+            //search
             if (resultCode == RESULT_OK) {
+                Toast.makeText(MainActivity.this, "l44444a", Toast.LENGTH_LONG).show();
                 Place place = PlaceAutocomplete.getPlace(this, data);
-                Log.i(TAG, "Place: " + place.getName());
+                Log.i(TAG, "Place:++++ " + place.getName());
             } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
                 Status status = PlaceAutocomplete.getStatus(this, data);
                 // TODO:Handle the error.
@@ -85,9 +214,16 @@ public class MainActivity extends FragmentActivity implements GoogleApiClient.On
             }
         }
     }
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions().position(sydney)
+                .title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
     // TODO: Please implement GoogleApiClient.OnConnectionFailedListener to
